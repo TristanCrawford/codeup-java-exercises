1f20b7a5d70dc5e9c16f47597e7085c31fe43cb4
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-02-27 11:34:33 -0600

    FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,36 +1,7 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-
-        // while
-
-        int a = 5;
-
-        while (a <= 15) {
-            System.out.printf("%d ", a);
-            a++;
-        }
-
-        System.out.println();
-
-        // do-while
-
-        int b = 2;
-
-        do {
-            System.out.println(b);
-            b = (int) Math.pow(b, 2);
-        } while ( b <= 1000000 );
-
-        // for
-
-        for (int i = 5; i <= 15; i++) {
-            System.out.printf("%d ", i);
-        }
-
-        System.out.println();
-
-        for (int i = 2; i <= 1000000; i = (int) Math.pow(i, 2) ) {
-            System.out.println(i);
+        for (int i = 1; i < 100; i++) {
+            System.out.println(((i % 5) == 0 && (i % 3) == 0) ? "FizzBuzz" : ((i % 3) == 0) ? "Fizz" : ((i % 5) == 0) ? "Buzz" : Integer.toString(i));
         }
     }
 }
