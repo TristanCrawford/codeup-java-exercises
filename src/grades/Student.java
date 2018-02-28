7f093f848f591ac9e0d38108e95f5fbee61c4d4d
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-02-28 12:30:54 -0600

    Student Class

---
+++ src/grades/Student.java
@@ -0,0 +1,38 @@
+package grades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sum = 0;
+        for( int grade : this.grades )
+            sum += grade;
+        return sum / this.grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student me = new Student("Tristan");
+        me.addGrade(5);
+        me.addGrade(10);
+        me.addGrade(15);
+        System.out.println(me.getGradeAverage());
+    }
+}
