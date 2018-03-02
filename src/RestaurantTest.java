04313b11fb46bd7033c46913373d9b64d90703d7
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-03-02 13:03:25 -0600

    Updated .gitignore + Refactored Restauraunt Exercises

--- src/RestaurantTest.java
+++ src/RestaurantTest.java
@@ -1,10 +1,7 @@
 public class RestaurantTest {
     public static void main(String[] args) {
-        RestaurauntDish dish = new RestaurauntDish();
-        dish.costInCents = 9001;
-        dish.nameOfDish = "Spicy Meatball";
-        dish.wouldRecommend = true;
-        System.out.printf("%s - %d ¢, %s\n", dish.nameOfDish, dish.costInCents, dish.wouldRecommend ? "10/10 Would Recommend" : "Would Not Recommend");
+        RestaurauntDish dish = new RestaurauntDish(350, "Spicy Meatball", true);
+        System.out.printf("%s - %d¢, %s\n", dish.getNameOfDish(), dish.getCostInCents(), dish.getWouldRecommend() ? "10/10 Would Recommend" : "Would Not Recommend");
         dish.eat();
     }
 }
--- src/RestaurauntDish.java
+++ src/RestaurauntDish.java
@@ -1,10 +1,40 @@
 public class RestaurauntDish {
 
-    public int costInCents;
-    public String nameOfDish;
-    public boolean wouldRecommend;
+    private int costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
 
     public void eat() {
         System.out.println("Nom Nom Nom!");
+    }
+
+    public RestaurauntDish(int costInCents, String nameOfDish, boolean wouldRecommend) {
+        this.costInCents = costInCents;
+        this.nameOfDish = nameOfDish;
+        this.wouldRecommend = wouldRecommend;
+    }
+
+    public int getCostInCents() {
+        return this.costInCents;
+    }
+
+    public String getNameOfDish() {
+        return this.nameOfDish;
+    }
+
+    public boolean getWouldRecommend() {
+        return this.wouldRecommend;
+    }
+
+    public void setCostInCents(int costInCents) {
+        this.costInCents = costInCents;
+    }
+
+    public void setNameOfDish(String nameOfDish) {
+        this.nameOfDish = nameOfDish;
+    }
+
+    public void setWouldRecommend(boolean wouldRecommend) {
+        this.wouldRecommend = wouldRecommend;
     }
 }
